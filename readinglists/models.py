b3d7b937f115f58a2e9e4e1bfb8b549e5046bbd3
MJR <mohammad.rafiei69@gmail.com>
2022-04-15 21:18:18 +0430

    add and use tags

--- readinglists/models.py
+++ readinglists/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class Document(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    description = models.TextField(default='')
+    description = models.TextField(default='', blank=True)
     url = models.CharField(max_length=600)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.name
--- readinglists/views.py
+++ readinglists/views.py
@@ -5,17 +5,23 @@ from .models import Document
 
 def add_document(request):
     if request.method == 'POST':
-        Document.objects.create(
+        doc = Document.objects.create(
             name=request.POST.get('name'),
             url=request.POST.get('url'),
             description=request.POST.get('description'),
             user=request.user
         )
+        new_tags = request.POST.get('tags').split()
+        doc.tags.add(*new_tags)
         return redirect(reverse('add-document'))
 
     return render(request, 'readinglists/add.html', {})
 
 
 def list_documents(request):
-    docuements = Document.objects.all()
+    tag = request.GET.get('tag')
+    if tag:
+        docuements = Document.objects.filter(tags__name=tag)
+    else:
+        docuements = Document.objects.all()
     return render(request, 'readinglists/list.html', {'documents': docuements})
